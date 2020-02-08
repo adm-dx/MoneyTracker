0649c2a0c181b0542ea277c55a01db32fdb21159
Roman Hohlov <adm.dx@outlook.com>
2020-02-08 23:57:48 +0300

    homework1

--- app/src/main/java/com/romanmiller/moneytracker/MainActivity.java
+++ app/src/main/java/com/romanmiller/moneytracker/MainActivity.java
@@ -1,8 +1,9 @@
 package com.romanmiller.moneytracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MainActivity", "onStop");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Main Activity", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Main Activity", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Main Activity", "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Main Activity", "onDestroy");
     }
 }
